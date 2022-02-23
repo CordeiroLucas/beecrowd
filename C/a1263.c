ad419edf8728f0ce16b902512d82f4f867c14fc7
CordeiroLucas <lcp2@cesar.school>
2022-02-23 13:51:02 -0300

    update

---
+++ C/a1263.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+	pegar apenas as primeiras letras das palavras
+*/
+
+int main(void)
+{
+	char palavra[50], letra0, letra1;
+	char texto[5000];
+
+	while (scanf(" %c", ))
+	
+	
+
+
+
+	return 0;
+}
